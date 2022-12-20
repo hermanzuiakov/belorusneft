1cb6d3ca0fd77d77c4cfdd40deabc5f8e95e0847
Herman Zuiakov <gzuyakov@gmail.com>
2022-12-20 20:48:01 +0100

    bug fix

---
+++ js/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let scrollPos = 0;
+const toTopButton = document.getElementById("toTop");
+
+function checkPosition() {
+    let windowY = window.scrollY;
+    if (windowY > scrollPos) {
+        toTopButton.classList.add("show");
+    } else {
+        toTopButton.classList.remove("show");
+    }
+    scrollPos = windowY;
+}
+
+function toTop() {
+    /*document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera*/
+
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+window.addEventListener('scroll', checkPosition);
+toTopButton.onclick = function () {
+    toTop();
+}
